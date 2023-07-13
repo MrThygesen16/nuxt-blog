c626497271f88a120f44afe4484a6ad45c42f24f
Mike <msthygesen16@gmail.com>
2023-07-13 20:24:52 +1000

    PLS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
-module.exports = {
-  router: {
-    base: '/nuxt-blog/'
+export default {
+ 
+  env: {
+    baseUrl: process.env.BASE_URL || 'https://mrthygesen16.github.io'
   },
 
   extends: 'content-wind',
@@ -10,10 +11,48 @@ module.exports = {
   components: true,
   // ssr: false,
   target: 'static',
+
+  head: {
+    htmlAttrs: {
+      lang: 'en'
+    },
+    title: 'Michael Thygesen',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: "Michael Thygesen's personal website"
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
   
   server: {
     port: 3000,
     host: '0.0.0.0'
+  },
+
+  sitemap: {
+    hostname: 'https://mrthygesen16.github.io',
+    routes: async () => {
+      const { $content } = require('@nuxt/content')
+
+      const posts = await $content({ deep: true })
+        .only(['path', 'draft'])
+        .where({ draft: { $ne: true } })
+        .fetch()
+      const projects = await $content('projects').only(['path']).fetch()
+
+      return []
+        .concat(
+          ...posts
+            .filter((x: { path: string }) => !x.path.startsWith('/projects/'))
+            .map((w: { path: any }) => w.path)
+        )
+        .concat(...projects.map((p: { path: any }) => p.path))
+    }
   },
 
 }
